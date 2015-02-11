bf880f584514f8118983516f730b216243c794b9
Noa Rave <noa.rave@berlinonline.de>
2015-02-11 12:55:02 +0100

    more functions

---
+++ 09_more_on_functions.php
@@ -0,0 +1,41 @@
+<?php
+
+function print_array(array $values)
+{
+    foreach($values as $value) {
+        echo $value . PHP_EOL;
+    }
+}
+
+class Foo
+{
+    protected $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+function print_foo(Foo $f)
+{
+    echo $f->getName();
+}
+
+$a = [1, 2, 3, 4];
+$b = new Foo("some_name\n");
+
+print_array($a);
+
+
+
+
+print_foo($b);
+
+print_array("this is a string");
+
+?>
